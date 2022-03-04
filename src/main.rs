81a39d54b80de45b3d6046fe467ced95e00dd846
imlyzh <enterindex@gmail.com>
2022-03-04 14:15:13 +0800

    update add utils and bitfield

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+#![feature(unchecked_math)]
 mod memory;
 mod monitor;
 mod abstract_machine;
 mod isa;
+mod utils;
 
 fn main() {
     println!("Hello, world!");
---
+++ src/utils.rs
@@ -0,0 +1,28 @@
+use std::{mem::size_of, ops::{Shl, Shr}};
+
+
+pub struct BitField<const LEFT: usize, const RIGHT: usize> {
+    pub value: usize,
+}
+
+impl<const LEFT: usize, const RIGHT: usize> BitField<LEFT, RIGHT> {
+    pub fn new(value: usize) -> Self {
+        Self { value }
+    }
+
+    pub fn get(&self) -> usize {
+        let lshift = size_of::<usize>()*8 - LEFT - 1;
+        let rshift = lshift + RIGHT;
+        unsafe {
+            self.value.unchecked_shl(lshift).unchecked_shr(rshift)
+        }
+    }
+}
+
+#[test]
+fn bitfield_test() {
+    let bf = BitField::<5, 0>::new(0b1010101010101010);
+    assert_eq!(bf.get(), 0b101010);
+    let bf = BitField::<10, 6>::new(0b1010101010101010);
+    assert_eq!(bf.get(), 0b01010);
+}
